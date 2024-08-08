b3bfbfdd2966ff9390689acb9b8e1f03416316f1
Robert Burston <robbieburston@gmail.com>
2024-08-08 16:18:52 +0200

    Add code to return complex string

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,6 +1,7 @@
 // https://expressjs.com/en/started/hello-world.html
 const express = require('express');
 const app = express();
+const urlprefix = '/api';
 
 // https://expressjs.com/en/4x/api.html#app.get
 app.get('/', (req, res) => {
@@ -10,6 +11,34 @@ app.get('/', (req, res) => {
 
 app.get('/test', (req, res) => {
     res.send('Hello World Express /test')
+});
+
+// https://www.json.org/json-en.html
+// {} = object
+// [] = array
+app.get(urlprefix+'/orders', (req, res) => {
+    const orders = [
+        {
+            id:"1",
+            name: "Orange"
+        },
+
+        {
+            id:"2",
+            name: "Banana"
+        },
+
+        {
+            id:"3",
+            name: "Pear"
+        }
+    ]
+    res.json(
+        {
+            message: "Fruits",
+            orders: orders
+        }
+    )
 });
 
 module.exports = app;
