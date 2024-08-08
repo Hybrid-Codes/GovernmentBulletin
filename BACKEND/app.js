8c4400e1650d87bdb5bacf6ce482bd617f3ce26f
Robert Burston <robbieburston@gmail.com>
2024-08-08 16:49:29 +0200

    Add database connection code

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -2,16 +2,34 @@
 const express = require('express');
 const app = express();
 const urlprefix = '/api';
+const mongoose = require('mongosoe');
+const Fruit = require('.module/fruit');
+const fs = require('fs');
+const cert = fs.readFileSync('keys/certificate.pem');
+const options = {
+    server: {sslCA: cert}};
+const connString = 'mongodb+srv://st10086237:auZt2d10HnN6NH8p@cluster0.tq6azhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.connect(connString)
+.then(()=>
+{
+    console.log('Connected :-)' )
+})
+.catch(()=>
+{
+    console.log('NOT connected :-(')
+},options);
+
+app.use(express.json());
 
 // https://expressjs.com/en/4x/api.html#app.get
-app.get('/', (req, res) => {
-    res.send('Hello World Express')
-});
+    // app.get('/', (req, res) => {
+    //     res.send('Hello World Express')
+    // });
 
-
-app.get('/test', (req, res) => {
-    res.send('Hello World Express /test')
-});
+    // app.get('/test', (req, res) => {
+    //     res.send('Hello World Express /test')
+    // });
 
 // https://www.json.org/json-en.html
 // {} = object
@@ -39,6 +57,20 @@ app.get(urlprefix+'/orders', (req, res) => {
             orders: orders
         }
     )
+});
+
+app.post(urlprefix+'/fruits', (req, res) => {
+    const fruit = new Fruit (
+        {
+            id: req.body.id,
+            name: req.body.name
+        }
+    );
+    fruit.save();
+    res.status(201).json({
+        message: 'Fruit created',
+        fruit: fruit
+    });
 });
 
 module.exports = app;
